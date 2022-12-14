f1edf854dce3a32057c8c65ed8481c630bdc9504
Taiveyon Shaw <shawt6@wwu.edu>
2022-12-13 21:03:23 -0800

    day 6 finished

---
+++ day6/day6.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class day6 {
+    public static void main(String[] args) throws Exception {
+            
+        // initialize variables
+        File file = new File("input.txt");
+        Scanner myReader = new Scanner(file);
+        boolean again = true;
+        boolean br = false;
+        String substr = "";
+        int iter = 3;
+
+        // Loop through every 4 sets
+        String str = myReader.nextLine();
+
+        // 4 letters
+        for (int i = 0; again; i++) { 
+            iter++;
+            substr = str.substring(i, i+4);
+            char[] strarr = substr.toCharArray();
+
+            // Compare
+            // How can we make it so it only iterates through the characters that's after the current one?
+            for (int j = 0; j < 4; j++) {
+                for (int k = j + 1; k <= 3; k++) {
+                    if (strarr[j] == strarr[k]) {
+                        br = true;
+                        break;
+                    }
+                }
+                if (br) {
+                    break;
+                }
+            }
+            if (br) {
+                br = false;
+                continue;
+            }
+            again = false;
+        }
+        System.out.println(substr);
+        System.out.println(iter);
+    }
+}
---
+++ day7/day7.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class day7 {
+    public static void main(String[] args) throws Exception {
+            
+        // initialize variables
+        File file = new File("input.txt");
+        Scanner myReader = new Scanner(file);
+
+        
+    }
+}
